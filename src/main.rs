68765329dc47667dd3b2c55d8509b7a5a230b581
djakish <69299717+djakish@users.noreply.github.com>
2022-01-13 19:13:15 +0600

    updated poise settings

--- src/main.rs
+++ src/main.rs
@@ -15,52 +15,36 @@ async fn main() {
     let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
 
     let options = poise::FrameworkOptions {
+        commands: vec![
+            play(),
+            pause(),
+            resume(),
+            skip(),
+            clear(),
+            disconnect(),
+            song(),
+            songqueue(),
+            repeat(),
+            switch(),
+            remove(),
+            seek(),
+        ],
         ..Default::default()
     };
     poise::Framework::build()
         .token(token)
         .options(options)
-        .command(play(), |f| f)
-        .command(pause(), |f| f)
-        .command(resume(), |f| f)
-        .command(skip(), |f| f)
-        .command(clear(), |f| f)
-        .command(disconnect(), |f| f)
-        .command(song(), |f| f)
-        .command(songqueue(), |f| f)
-        .command(repeat(), |f| f)
-        .command(switch(), |f| f)
-        .command(remove(), |f| f)
-        .command(seek(), |f| f)
         .client_settings(|client| client.register_songbird())
         .user_data_setup(move |_ctx, _ready, _framework| {
             // This part of for registering the commands on the start of the bot
             Box::pin(async move {
-                let token = env::var("REGISTER").expect("Expected token for registartion");
-                if token == "false" {
-                    return Ok(());
-                }
                 let guild_id = env::var("GUILD_ID").expect("Expected guild ID");
-                let mut commands_builder =
-                    poise::serenity_prelude::CreateApplicationCommands::default();
-                let commands = &_framework.options().application_options.commands;
-
-                for cmd in commands {
-                    let desc;
-                    match cmd {
-                        poise::ApplicationCommandTree::Slash(slash_command) => {
-                            desc = slash_command.description();
-                            println!("{}", desc);
-                            println!("{}", slash_command.name());
-                        }
-                        poise::ApplicationCommandTree::ContextMenu(_) => todo!(),
-                    }
-                    commands_builder
-                        .create_application_command(|f| cmd.create(f).description(desc));
-                }
+                let mut commands_builder = serenity::builder::CreateApplicationCommands::default();
+                let command_to_reg = register().create_as_slash_command().unwrap();
+                commands_builder.add_application_command(command_to_reg);
                 let commands_builder = serenity::json::Value::Array(commands_builder.0);
                 _ctx.http
-                    .create_guild_application_commands(
+                    .create_guild_application_command(
                         guild_id.parse::<u64>().unwrap(),
                         &commands_builder,
                     )
@@ -71,4 +55,12 @@ async fn main() {
         .run()
         .await
         .unwrap();
+}
+
+/// Register commands in guild
+#[poise::command(slash_command)]
+pub async fn register(ctx: Context<'_>) -> Result<(), Error> {
+    poise::builtins::register_application_commands(ctx,false).await?;
+
+    Ok(())
 }
